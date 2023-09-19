642b9c9bcfde3400ab4fcb2d9ad799c6ee7d33f1
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-09-19 12:01:08 -0400

    integrated flask_moment and added timestamps to user.html

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -10,7 +13,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
