13cef508557d3b9a3b02bea9ec2a7f21b2d1d70b
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-09-19 13:36:32 -0400

    added wtf form which handles name and uoft email

--- hello.py
+++ hello.py
@@ -2,14 +2,33 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Regexp
 
+# initialize the app
 app = Flask(__name__)
+
+# initialize extensions
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+# Flask-WTF config
+app.config['SECRET_KEY'] = '3123N12K3NOD12N3IJ12CJIK123CNL12N3'
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    email = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+        email = form.email.data
+        form.email.data = ''
+    return render_template('index.html', form=form, name=name, email=email)
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,3 +41,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField("What is your UofT Email address?", validators=[DataRequired(), 
+                                                                            Regexp('.*utoronto\.ca.*', 0, 'Email must be a UofT email address')])
+    submit = SubmitField('Submit')
+
