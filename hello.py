e86cc3f16937f88616921e84b87d77d4c4f6ca63
Sebastian Czyrny <sebastian.czyrny@hotmail.com>
2023-09-19 13:46:00 -0400

    added wtf form to index.html (example 4-7)

--- hello.py
+++ hello.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = '23J1CJIO23NO12IJL1NL'
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,3 +32,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
